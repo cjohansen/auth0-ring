dc86ac131fe6a9a127aed52809f86cc31a16b94d
Christian Johansen <christian@cjohansen.no>
2017-02-09 11:34:43 +0100

    The middleware shouldn't clobber the auth callback cookies

--- src/auth0_ring/middleware.clj
+++ src/auth0_ring/middleware.clj
@@ -9,8 +9,8 @@
       (let [id-token (get-in req [:cookies "id-token" :value])
             access-token (get-in req [:cookies "access-token" :value])]
         (if (and id-token access-token)
-          (if-let [user (keywordize-keys (verify-token jwt-verifier id-token))]
-            (handler (assoc req :user user))
-            (update-in (handler req) [:cookies] #(merge % {"id-token" (delete-cookie req)
-                                                           "access-token" (delete-cookie req)})))
+          (if-let [user (verify-token jwt-verifier id-token)]
+            (handler (assoc req :user (keywordize-keys (into {} user))))
+            (update-in (handler req) [:cookies] #(merge {"id-token" (delete-cookie req)
+                                                         "access-token" (delete-cookie req)} %)))
           (handler req))))))
