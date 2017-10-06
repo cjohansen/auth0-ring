159aea18c7ac80c8e5d58428f6f1602fd601dc08
Christian Karlsen <christian@eventum.no>
2017-10-06 15:34:56 +0200

    Set expires cookies. Might fix issue with persistent cookies on legacy IE

--- src/auth0_ring/core.clj
+++ src/auth0_ring/core.clj
@@ -1,5 +1,20 @@
 (ns auth0-ring.core
-  (:import [java.net URLEncoder]))
+  (:import [java.net URLEncoder]
+           (java.text SimpleDateFormat)
+           (java.util Calendar TimeZone)))
+
+(def rfc822-formatter
+  (doto
+    (SimpleDateFormat. "EEE, dd MMM yyyy HH:mm:ss Z")
+    (.setTimeZone (TimeZone/getTimeZone "GMT"))))
+
+(defn max-age->expires
+  "Return a valid 'expires' value (RFC822 string)"
+  [max-age]
+  (let [cal (doto
+              (Calendar/getInstance)
+              (.add Calendar/SECOND max-age))]
+    (.format rfc822-formatter (.getTime cal))))
 
 (defn urlencode [str]
   (URLEncoder/encode str "utf-8"))
@@ -16,9 +31,11 @@
          (str "&returnTo=" (urlencode (qualify-url req url))))))
 
 (defn http-only-cookie [req cookie]
-  (merge {:http-only true
-          :secure (= (:scheme req) :https)
-          :path "/"} cookie))
+  (cond-> (merge {:http-only true
+                  :secure (= (:scheme req) :https)
+                  :path "/"} cookie)
+          (number? (:max-age cookie))
+          (assoc :expires (max-age->expires (:max-age cookie)))))
 
 (defn delete-cookie [req]
   (http-only-cookie req {:value "" :max-age 1}))
